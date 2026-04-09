774277b0bd5fbcacd77e1f2e63f42dd978555325
Burak <buurakcelik@gmail.com>
2026-04-09 15:06:50 +0300

    Update install_page.dart

--- flutter/lib/desktop/pages/install_page.dart
+++ flutter/lib/desktop/pages/install_page.dart
@@ -187,9 +187,9 @@ class _InstallPageBodyState extends State<_InstallPageBody>
                           InkWell(
                             hoverColor: Colors.transparent,
                             onTap: () => launchUrlString(
-                                'https://rustdesk.com/privacy.html'),
+                                'https://bcsistem.com/gizlilik.html'),
                             child: Tooltip(
-                              message: 'https://rustdesk.com/privacy.html',
+                              message: 'https://bcsistem.com/gizlilik.html',
                               child: Row(children: [
                                 Icon(Icons.launch_outlined, size: 16)
                                     .marginOnly(right: 5),
